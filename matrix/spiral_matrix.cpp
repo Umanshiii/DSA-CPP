8faa6f558dadbe27ee52e3e8b2f4c30e61458c6e
Umanshi <umanshi121@gmail.com>
2026-09-22 01:04:38 +0530

    spiral matrix

--- matrix/spiral_matrix.cpp
+++ matrix/spiral_matrix.cpp
@@ -4,44 +4,43 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) 
     {
-        int top=0;
-        int left=0;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int right=m-1;
-        int bottom=n-1;
-        vector<int>v;
-        int count=0;
-        int total=n*m;
-        while(count<total)
-        {    for(int i=left; i<=right; i++)
-            {
+        int top = 0;
+        int left = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int right = m - 1;
+        int bottom = n - 1;
+        vector<int> v;
+        
+        while (top <= bottom && left <= right) 
+        {    
+            // 1. Traverse top row from left to right
+            for (int i = left; i <= right; i++) {
                 v.push_back(matrix[top][i]);
-                count+=1;
             }
-            if(count==total)
-            {
-                break;
-            }
-            top+=1;
-            for(int i=top; i<=bottom; i++)
-            {
+            top++;
+            
+            // 2. Traverse right column from top to bottom
+            for (int i = top; i <= bottom; i++) {
                 v.push_back(matrix[i][right]);
-                count+=1;
             }  
-            right-=1;
-            for(int i=right; i>=left; i--) 
-            {
-                v.push_back(matrix[bottom][i]);
-                count+=1;
+            right--;
+            
+            // 3. Traverse bottom row from right to left (only if rows remain)
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    v.push_back(matrix[bottom][i]);
+                }
+                bottom--;
             }
-            bottom-=1;
-            for(int i=bottom; i>=top; i--)
-            {
-                v.push_back(matrix[i][left]);
-                count+=1;
+            
+            // 4. Traverse left column from bottom to top (only if columns remain)
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    v.push_back(matrix[i][left]);
+                }
+                left++;
             }
-            left+=1;
         }  
         return v;
     }

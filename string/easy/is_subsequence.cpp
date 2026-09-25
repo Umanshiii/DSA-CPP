ddc64b4c7e3bfd5c3c8980d2f1d4c7eb32efbe8f
Umanshi <umanshi121@gmail.com>
2026-09-25 22:58:36 +0530

    is subsequence

---
+++ string/easy/is_subsequence.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/is-subsequence
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < t.length() && j < s.length()) {
+            if (t[i] == s[j]) {
+                j++;
+            }
+            i++;
+        }
+        return j == s.length();
+    }
+};
